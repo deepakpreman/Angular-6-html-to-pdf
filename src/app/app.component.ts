426d37eb3607c734f9c54ba7f8716511c6c5682c
M <DM5875@zebra.com>
2019-01-11 12:21:27 +0530

    adding files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import * as jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+import * as saveAs from 'file-saver'; 
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  
+  downloadPdf(){
+    var data = document.getElementById('container'); 
+    html2canvas(data).then(canvas => { 
+    var imgWidth = 208; 
+    var imgHeight = canvas.height * imgWidth / canvas.width;    
+    const contentDataURL = canvas.toDataURL('image/png') 
+    let pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF 
+    var position = 10; 
+    pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)     
+    pdf.save('htmltopdf.pdf'); // Generated PDF  
+    })
+  }
 }
+ 
